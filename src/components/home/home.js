98de95a5bbc6d2ec252c16be1459b72a9c3d8a41
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2023-06-26 14:42:50 +0600

    first commit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import "./home.css"
-import Header from "../Header/Header";
-import Country from "../country/country";
+
 const Home = () => {
     return (
         <div id="home">
-            <Header>
-               <Country/>
-            </Header>
+
         </div>
     );
 };
